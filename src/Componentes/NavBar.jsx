355340f330767d391df7745eac3d404991fe1a87
mezaaziel06 <mezzaaazielle@gmail.com>
2024-01-28 08:12:13 -0600

    cambio

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -9,28 +9,29 @@ function NavBar() {
   return (
     <nav className="bg-green-800 h-screen w-64 fixed top-0 left-0 flex flex-col text-white p-4">
       <div className="font-bold text-lg mb-4 flex items-center">  Nombre Empresa/Logo </div>
-      <ul className="flex flex-col space-y-2">
-      <li className="flex items-center text-center mb-3 hover:bg-teal-500 transition duration-300 ease-in-out">
-          <FontAwesomeIcon icon={faHome} className="ml-6" />
-          Dashboard
-        </li>
-        <li className="flex items-center text-center mb-3 hover:bg-teal-500 transition duration-300 ease-in-out">
-          <FontAwesomeIcon icon={faFolderOpen} className="ml-6" />
-          Proyectos
-        </li>
-        <li className="flex items-center text-center mb-3 hover:bg-teal-500 transition duration-300 ease-in-out">
-          <FontAwesomeIcon icon={faUsers} className="ml-6" />
-          Equipos
-        </li>
-        <li className="flex items-center text-center mb-3 hover:bg-teal-500 transition duration-300 ease-in-out">
-          <FontAwesomeIcon icon={faToolbox} className="ml-6" />
-          Materiales
-        </li>
-        <li className="flex items-center text-center mb-3 hover:bg-teal-500 transition duration-300 ease-in-out">
-          <FontAwesomeIcon icon={faUserFriends} className="ml-6" />
-          Empleados
-        </li>
-      </ul>
+      <ul className="list-none p-0">
+      <li className="flex items-center text-center my-6 hover:bg-teal-500 transition duration-300 ease-in-out">
+  <FontAwesomeIcon icon={faHome} className="ml-3 mr-2" />
+  Dashboard
+</li>
+  <li className="flex items-center text-center my-6 hover:bg-teal-500 transition duration-300 ease-in-out">
+    <FontAwesomeIcon icon={faFolderOpen} className="ml-3 mr-2" />
+    Proyectos
+  </li>
+  <li className="flex items-center text-center my-6 hover:bg-teal-500 transition duration-300 ease-in-out">
+    <FontAwesomeIcon icon={faUsers} className="ml-3 mr-2" />
+    Equipos
+  </li>
+  <li className="flex items-center text-center my-6 hover:bg-teal-500 transition duration-300 ease-in-out">
+    <FontAwesomeIcon icon={faToolbox} className="ml-3 mr-2" />
+    Materiales
+  </li>
+  <li className="flex items-center text-center my-6 hover:bg-teal-500 transition duration-600 ease-in-out">
+    <FontAwesomeIcon icon={faUserFriends} className="ml-3 mr-2" />
+    Empleados
+  </li>
+</ul>
+
     </nav>
     
    
