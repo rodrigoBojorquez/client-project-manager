8a19f4d044a439655446ca8f25d91339b096bd98
Alexander <alexdroide466@gmail.com>
2024-02-02 08:09:35 -0500

    Crecion de empleado

--- src/Pages/Employees/Forms/CreateEmployees.jsx
+++ src/Pages/Employees/Forms/CreateEmployees.jsx
@@ -63,21 +63,21 @@ function CreateEmployees({ closeModal }) {
     }
     if (user.email.trim() === "") {
       valid = false;
-      newErrors.email = "Por favor, ingresa una cantidad";
+      newErrors.email = "Por favor, ingresa un email";
     } else if (!expresion.test(user.email.trim())) {
-      newErrors.email = "La cantidad no pude ser un caracter";
+      newErrors.email = "El email no pude ser un caracter";
     }
     if (user.speciality.trim() === "") {
       valid = false;
-      newErrors.speciality = "Por favor, ingresa una cantidad";
+      newErrors.speciality = "Por favor, ingresa una especialidad";
     } else if (!expresion.test(user.speciality.trim())) {
-      newErrors.speciality = "La cantidad no pude ser un caracter";
+      newErrors.speciality = "La especialidad no pude ser un caracter";
     }
     if (user.rol.trim() === "") {
       valid = false;
-      newErrors.rol = "Por favor, ingresa una cantidad";
+      newErrors.rol = "Por favor, ingresa un rol";
     } else if (!expresion.test(user.rol.trim())) {
-      newErrors.rol = "La cantidad no pude ser un caracter";
+      newErrors.rol = "El rol no pude ser un caracter";
     }
     setError(newErrors);
     return valid;
@@ -90,9 +90,10 @@ function CreateEmployees({ closeModal }) {
         const response = await axios.post(
           "http://localhost:8000/project-manager/warehouse",
           {
-            materialName: material.name,
-            quantity: material.quantity,
-            measure: "unidades",
+            username: user.username,
+            email: user.email,
+            speciality: user.speciality,
+            rol: user.rol,
           },
           {
             headers: {
@@ -101,7 +102,7 @@ function CreateEmployees({ closeModal }) {
           }
         );
 
-        console.log("Formulario enviado", material);
+        console.log("Formulario enviado", user);
         console.log("Respuesta del servidor:", response.data);
         closeModal();
       } catch (error) {
@@ -113,66 +114,75 @@ function CreateEmployees({ closeModal }) {
   };
   return (
     <div className="h-screen w-full flex justify-center items-center bg-black bg-opacity-65 backdrop-blur-sm font-Nunito fixed left-0 top-0 ">
-      <div className="bg-white h-auto w-auto rounded-md grid">
-        <form action="" className="flex flex-col m-10 gap-10">
+      <div className="bg-white h-[500px] w-auto rounded-md grid">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-around m-10 gap-10"
+        >
           <h2 className="text-4xl font-semibold">Nuevo Empleado</h2>
-          <div className="flex flex-col">
+          <div className="flex gap-10">
+            <div>
+              <p className="text-xl text-[#666] font-semibold">Nombres *</p>
+              <input
+                value={user.username}
+                onChange={handleChangeUserName}
+                type="text"
+                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
+              />
+              <p className="text-sm text-red-600">{error.username}</p>
+            </div>
+            <div>
+              <p className="text-xl text-[#666] font-semibold">Email</p>
+              <input
+                value={user.email}
+                onChange={handleChangeEmail}
+                type="email"
+                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
+              />
+              <p className="text-sm text-red-600">{error.email}</p>
+            </div>
+          </div>
+          <div className="flex gap-10">
+            <div>
+              <p className="text-xl text-[#666] font-semibold">
+                Especialidad *
+              </p>
+              <input
+                value={user.speciality}
+                onChange={handleChangeSpeciality}
+                type="text"
+                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
+              />
+              <p className="text-sm text-red-600">{error.speciality}</p>
+            </div>
+
             <div className="flex gap-16">
               <div>
-                <p className="text-xl text-[#666] font-semibold">Nombres *</p>
+                <p className="text-xl text-[#666] font-semibold">Rol *</p>
                 <input
+                  value={user.rol}
+                  onChange={handleChangeRol}
                   type="text"
                   className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
                 />
-              </div>
-              <div>
-                <p className="text-xl text-[#666] font-semibold">Apellidos *</p>
-                <input
-                  type="text"
-                  className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
-                />
+                <p className="text-sm text-red-600">{error.rol}</p>
               </div>
             </div>
           </div>
-          <div className="flex gap-16">
-            <div>
-              <p className="text-xl text-[#666] font-semibold">RFC *</p>
-              <input
-                type="text"
-                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
-              />
-            </div>
-            <div>
-              <p className="text-xl text-[#666] font-semibold">Telefono *</p>
-              <input
-                type="text"
-                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
-              />
-            </div>
-          </div>
-          <div className="flex gap-16">
-            <div>
-              <p className="text-xl text-[#666] font-semibold">Correo *</p>
-              <input
-                type="text"
-                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
-              />
-            </div>
-            <div>
-              <p className="text-xl text-[#666] font-semibold">Especialidad</p>
-              <input
-                type="text"
-                className="w-[250px] h-8 px-2 outline-none border border-[#a9a9a9] rounded-md "
-              />
-            </div>
-          </div>
-          <div>
-            <button className="flex gap-2 items-center text-lg text-[#1DAF90] hover:text-white hover:bg-[#1DAF90] font-semibold border-2 border-[#1DAF90] px-2 py-1 rounded-md">
-              <p className="text-3xl" />
+          <div className="flex items-center justify-center gap-5">
+            <button
+              type="submit"
+              className="flex gap-2 items-center text-lg text-[#1DAF90] hover:text-white hover:bg-[#1DAF90] font-semibold border-2 border-[#1DAF90] px-2 py-1 rounded-md"
+            >
               Añadir
             </button>
+            <button
+              onClick={closeModal}
+              className="flex gap-2 items-center text-lg text-[#666] hover:text-white hover:bg-red-600 font-semibold border-2 border-[#666] hover:border-red-600 px-2 py-1 rounded-md"
+            >
+              Descartar
+            </button>
           </div>
-          <div className="flex items-center justify-center m-10 gap-6"></div>
         </form>
       </div>
     </div>
